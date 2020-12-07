8ddc64757625304ede744dbc2acc79b112dee79a
bas-kirill <people.can.you.can@gmail.com>
2020-12-07 14:24:47 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -34,30 +34,80 @@ void bubbleSort(int a[], int n) {
     }
 }
 
-void quickSort(int a[], int n, int l, int r) {
-    if (l >= r) {
-        ++quickSortComparsionsNumber;
-        return;
-    }
+void print(int a[], int n) {
+    for (int i = 0; i < n; ++i)
+        cout << a[i] << " ";
+}
 
-    int middle = l + (r - l) / 2;
+void quickSort(int *a, int left, int right) {
+    if (left >= right)
+        return;
+
+    int middle = left + (right - left) / 2;
     int pivot = a[middle];
 
-    int i = l, j = r;
-    while (i <= j) {
-        ++quickSortComparsionsNumber;
-        while (a[i] < pivot) { ++i; ++quickSortComparsionsNumber; } // <= -> <, coz 3 3 3
-        while (a[j] > pivot) { --j; ++quickSortComparsionsNumber; }
+    int i = left, j = right;
+    do {
+        while (i != middle && a[i] < pivot) { ++i; ++quickSortComparsionsNumber; }
+        while (j != middle && a[j] > pivot) { --j; ++quickSortComparsionsNumber; }
 
+        if (i != middle) ++quickSortComparsionsNumber;
+        if (j != middle) ++quickSortComparsionsNumber;
+        
         if (i <= j) {
-            ++quickSortComparsionsNumber;
-            ++quickSortPermutationsNumber;
-            swap(a[i++], a[j--]);
-        }
-    }
+            if (i != j) {
+                ++quickSortPermutationsNumber;
+                // swap
+                int tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp; 
+            }
 
-    quickSort(a, n, l, j);
-    quickSort(a, n, i, r);
+            if (i == middle) ++i;
+            if (j == middle) --j;
+
+            ++i, --j;
+        }
+    } while (i <= j);
+
+    quickSort(a, left, j);
+    quickSort(a, i, right);
+}
+
+void quick_sort_test(int m, int n, int *arr, int* count_com, int* count_per){
+
+    int i = m;
+    int j = n;
+
+    int temp;
+    int middle_ind = (i + j) /  2;
+    int middle = arr[middle_ind];
+
+    do{
+
+        while(i != middle_ind && arr[i] < middle) { i++; (*count_com)++;}
+        if(i != middle_ind) (*count_com)++;
+        while(j != middle_ind && arr[j] > middle) { j--; (*count_com)++;}
+        if(j != middle_ind) (*count_com)++;
+
+        if(i <= j){
+            if(i != j){
+                (*count_per)++;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            if (i == middle_ind) i++;
+            if (j == middle_ind) j--;
+            i++; j--;
+        }
+    }while(i <= j);
+
+
+
+    if(j > m) quick_sort_test(m, j, arr, count_com, count_per);
+    if(n > i) quick_sort_test(i, n, arr, count_com, count_per);
+
 }
 
 void merge(int a[], int l1, int r1, int l2, int r2) {
@@ -103,6 +153,7 @@ void insertionSort(int a[], int n) {
     for (int i = 1; i < n; ++i) {
         int idx = i;
         while (idx >= 1 && a[idx] < a[idx - 1]) {
+            cout << "Сейчас рассматривается элемент " << i << " " << idx << endl;
             insertionSortComparsionsNumber += 2;
             swap(a[idx], a[idx - 1]);
             ++insertionSortPermutationsNumber;
@@ -121,43 +172,51 @@ void fillRandomNumbers(int a[], int n) {
 }
 
 int main() {
-    int n;
-    int cnt = 0;
-    while (cnt <= 4) {
-        switch (cnt) {
-            case 0:
-                n = 1'000;
-                break;
-            case 1:
-                n = 5'000;
-                break;
-            case 2:
-                n = 10'000;
-                break;
-            case 3:
-                n = 20'000;
-                break;
-            case 4:
-                n = 50'000;
-                break;
-        }
-        int a[MAXN];
+    int a[] = {1, 2, 4, 3, 5};
+    int count_com = 0;
+    int count_per = 0;
+    quickSort(a, 0, 4);
+    cout << quickSortComparsionsNumber << " " << quickSortPermutationsNumber;
 
-        fillRandomNumbers(a, n);
-        bubbleSort(a, n);
-
-        fillRandomNumbers(a, n);
-        quickSort(a, n, 0, n - 1);
-
-        fillRandomNumbers(a, n);
-        insertionSort(a, n);
-
-        cout << "n = " << n << ":" << endl;
-        cout << '\t' << "Bubble sort: " << bubbleSortComparsionsNumber << " " << bubbleSortPermutationsNumber << endl;
-        cout << '\t' << "Quick sort:  " << quickSortComparsionsNumber << " " << quickSortPermutationsNumber << endl;
-        cout << '\t' << "Insertion sort:  " << insertionSortComparsionsNumber << " " << insertionSortPermutationsNumber << endl;
-
-        ++cnt;
-    }
+//    int aa[] = {1, 2, 4, 3, 5};
+//    insertionSort(aa, 5);
+//    int n;
+//    int cnt = 0;
+//    while (cnt <= 4) {
+//        switch (cnt) {
+//            case 0:
+//                n = 1'000;
+//                break;
+//            case 1:
+//                n = 5'000;
+//                break;
+//            case 2:
+//                n = 10'000;
+//                break;
+//            case 3:
+//                n = 20'000;
+//                break;
+//            case 4:
+//                n = 50'000;
+//                break;
+//        }
+//        int a[MAXN];
+//
+//        fillRandomNumbers(a, n);
+//        bubbleSort(a, n);
+//
+//        fillRandomNumbers(a, n);
+//        quickSort(a, n, 0, n - 1);
+//
+//        fillRandomNumbers(a, n);
+//        insertionSort(a, n);
+//
+//        cout << "n = " << n << ":" << endl;
+//        cout << '\t' << "Bubble sort: " << bubbleSortComparsionsNumber << " " << bubbleSortPermutationsNumber << endl;
+//        cout << '\t' << "Quick sort:  " << quickSortComparsionsNumber << " " << quickSortPermutationsNumber << endl;
+//        cout << '\t' << "Insertion sort:  " << insertionSortComparsionsNumber << " " << insertionSortPermutationsNumber << endl;
+//
+//        ++cnt;
+//    }
     return 0;
 }
